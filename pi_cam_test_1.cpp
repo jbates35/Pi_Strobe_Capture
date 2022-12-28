bb9491a01713e597305dc0dcac682f4a6ecdb58d
Jimmy Bates <70033294+jbates35@users.noreply.github.com>
2022-12-27 19:02:06 -0800

    Got GPIO Button ISR workin

--- pi_cam_test_1.cpp
+++ pi_cam_test_1.cpp
@@ -12,14 +12,31 @@
 
 #include <opencv2/opencv.hpp>
 
-#define BUTTON_1_PIN	19
-#define LED_PIN			26
+//Holds camera object for taking picture, saving to file, doing some processing
+#include "FishTestCamera.h"
+
+#define BUTTON_1_PIN	19 // pin for button
+#define LED_PIN			26 // pin for led
+
+#define ISR_TIMEOUT		100000 // microseconds
+
+#define DEBOUNCE_INTERVAL 0.01 // Amount of time to debounce (10ms)
+
+
+/*******	FUNCTION PROTOCOLS	*******/
 
 //Returns 0 if successful
 int init();
 
 //ISR for Button 1
-void button_1_isr();
+void button_1_isr(int gpio, int level, uint32_t tick);
+
+//
+void camera_shot();
+
+
+
+/*******	FUNCTION DEFINTIONS		*******/
 
 int main()
 {
@@ -32,7 +49,6 @@ int main()
 	while (1)
 	{
 		unsigned int led_val = gpioRead(BUTTON_1_PIN);
-		gpioWrite(LED_PIN, led_val);
 	}	
 }
 
@@ -47,16 +63,50 @@ int init()
 	
 	//LED Pin setup
 	gpioSetMode(LED_PIN, PI_OUTPUT);
+	gpioWrite(LED_PIN, 0);
 
-	//Button setup
+	//Button 1 setup
 	gpioSetMode(BUTTON_1_PIN, PI_INPUT);
 	gpioSetPullUpDown(BUTTON_1_PIN, PI_PUD_UP);
+	
+	//Setup ISR with Button 1 
+	gpioSetISRFunc(BUTTON_1_PIN, FALLING_EDGE, ISR_TIMEOUT, button_1_isr);	
 	
 	return 0;
 }
 
-//Takes picture of camera
-void button_1_isr()
-{
+//Set ISR for button 1
+void button_1_isr(int gpio, int level, uint32_t tick)
+{	
+	//Test counter
+	static bool button_pressed = false;
+	static double press_time = cv::getTickCount();
+	static bool led_state=false;
+
+	//If button is in debounce interval, return and do nothing
+	if (button_pressed == true && (cv::getTickCount() - press_time) / cv::getTickFrequency() < DEBOUNCE_INTERVAL)
+	{
+		return;
+	}
 	
+	//If button is pressed, make true and start timer
+	if (button_pressed == false) 
+	{
+		button_pressed = true;
+		
+		//Start timer
+		press_time = cv::getTickCount();
+		
+		return;
+	}
+	
+	//If debounce successful, run actual function
+	if (button_pressed == true && (cv::getTickCount() - press_time) / cv::getTickFrequency() >= DEBOUNCE_INTERVAL)
+	{
+		button_pressed = false;
+		
+		led_state = !led_state;
+		
+		gpioWrite(LED_PIN, !gpioRead(LED_PIN));
+	}	
 }
